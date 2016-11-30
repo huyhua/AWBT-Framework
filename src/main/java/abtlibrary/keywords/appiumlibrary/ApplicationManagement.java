91e616379258ffdacc115855e9ac3be08c2c4fbe
Hua Huy (NVG, Mobile QA) <huyhua@nhatvietgroup.com.vn>
2016-11-30 16:40:07 +0700

    download app from hockeyapps

--- src/main/java/abtlibrary/keywords/appiumlibrary/ApplicationManagement.java
+++ src/main/java/abtlibrary/keywords/appiumlibrary/ApplicationManagement.java
@@ -4,15 +4,29 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.Autowired;
 import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywordOverload;
 import org.robotframework.javalib.annotation.RobotKeywords;
 
 import com.applitools.eyes.Eyes;
@@ -22,6 +36,7 @@ import abtlibrary.ABTLibraryFatalException;
 import abtlibrary.RunOnFailureKeywordsAdapter;
 import abtlibrary.keywords.selenium2library.BrowserManagement;
 import abtlibrary.keywords.selenium2library.Logging;
+import abtlibrary.utils.HttpRequestUtils;
 
 @SuppressWarnings("deprecation")
 @RobotKeywords
@@ -38,7 +53,7 @@ public class ApplicationManagement extends RunOnFailureKeywordsAdapter {
 	 */
 	@Autowired
 	protected Logging logging;
-	
+
 	protected Eyes eyes;
 
 	// ##############################
@@ -51,7 +66,8 @@ public class ApplicationManagement extends RunOnFailureKeywordsAdapter {
 	@RobotKeyword
 	public void closeApplication() {
 		if (browserManagement.webDriverCache.getCurrentSessionId() != null) {
-			logging.debug(String.format("Closing application with session id %s",
+			logging.debug(String.format(
+					"Closing application with session id %s",
 					browserManagement.webDriverCache.getCurrentSessionId()));
 			browserManagement.webDriverCache.close();
 		}
@@ -137,36 +153,43 @@ public class ApplicationManagement extends RunOnFailureKeywordsAdapter {
 	 * @return Session Id
 	 */
 	@RobotKeyword
-	@ArgumentNames({ "remoteUrl", "alias", "appPath", "deviceName", "platformVersion" })
-	public String openApplication(String remoteUrl, String alias, String appPath, String deviceName,
-			String platformVersion) {
+	@ArgumentNames({ "remoteUrl", "alias", "appPath", "deviceName",
+			"platformVersion" })
+	public String openApplication(String remoteUrl, String alias,
+			String appPath, String deviceName, String platformVersion) {
 		WebDriver driver;
 		String platformName = appPath.contains(".apk") ? "Android" : "iOS";
-		
-		
+
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability("app", appPath);
 		cap.setCapability("platformName", platformName);
 		cap.setCapability("platformVersion", platformVersion);
 		cap.setCapability("deviceName", deviceName);
-		
 
 		try {
 			if (platformName.equals("Android")) {
 				driver = new AndroidDriver<>(new URL(remoteUrl), cap);
-				
-			} else{
+
+			} else {
 				cap.setCapability("autoAcceptAlerts", false);
 				driver = new IOSDriver<>(new URL(remoteUrl), cap);
 			}
 
-			String sessionId = browserManagement.webDriverCache.register(driver, alias, platformName);
-			logging.debug(String.format("Openning '%s' driver to base application '%s'", platformName, appPath));
-			driver.manage().timeouts().implicitlyWait((int) (browserManagement.implicitWait * 1000.0),
-					TimeUnit.MILLISECONDS);
+			String sessionId = browserManagement.webDriverCache.register(
+					driver, alias, platformName);
+			logging.debug(String.format(
+					"Openning '%s' driver to base application '%s'",
+					platformName, appPath));
+			driver.manage()
+					.timeouts()
+					.implicitlyWait(
+							(int) (browserManagement.implicitWait * 1000.0),
+							TimeUnit.MILLISECONDS);
 			return sessionId;
 		} catch (Throwable t) {
-			logging.warn(String.format("Openning '%s' driver to base application '%s' failed", platformName, appPath));
+			logging.warn(String.format(
+					"Openning '%s' driver to base application '%s' failed",
+					platformName, appPath));
 			throw new ABTLibraryFatalException(t);
 		}
 	}
@@ -178,9 +201,10 @@ public class ApplicationManagement extends RunOnFailureKeywordsAdapter {
 	 *            specified text.
 	 */
 	@RobotKeyword
-	@ArgumentNames({"text"})
+	@ArgumentNames({ "text" })
 	public WebElement scrollTo(String text) {
-		return ((AppiumDriver<?>) browserManagement.getCurrentWebDriver()).scrollTo(text);
+		return ((AppiumDriver<?>) browserManagement.getCurrentWebDriver())
+				.scrollTo(text);
 	}
 
 	/**
@@ -190,22 +214,134 @@ public class ApplicationManagement extends RunOnFailureKeywordsAdapter {
 	 *            specified text.
 	 */
 	@RobotKeyword
-	@ArgumentNames({"text"})
+	@ArgumentNames({ "text" })
 	public WebElement scrollToExact(String text) {
-		return ((AppiumDriver<?>) browserManagement.getCurrentWebDriver()).scrollToExact(text);
+		return ((AppiumDriver<?>) browserManagement.getCurrentWebDriver())
+				.scrollToExact(text);
 	}
 
 	@RobotKeyword
-	@ArgumentNames({"APIKey", "matchLevel", "deviceName"})
-	public void enableApplitool(String APIKey, String matchLevel, String deviceName){
-				eyes = new Eyes();
-				//eyes.setApiKey("Eyt18cF9exK4109txbzzE2ij0isWh8D2zFdts3vYVOhIg110");
-				eyes.setApiKey(APIKey);
-				eyes.setMatchLevel(MatchLevel.valueOf(matchLevel));
-				eyes.setHostOS(deviceName);
-				
+	@ArgumentNames({ "APIKey", "matchLevel", "deviceName" })
+	public void enableApplitool(String APIKey, String matchLevel,
+			String deviceName) {
+		eyes = new Eyes();
+		// eyes.setApiKey("Eyt18cF9exK4109txbzzE2ij0isWh8D2zFdts3vYVOhIg110");
+		eyes.setApiKey(APIKey);
+		eyes.setMatchLevel(MatchLevel.valueOf(matchLevel));
+		eyes.setHostOS(deviceName);
+
 	}
 	
+	@RobotKeywordOverload
+	@ArgumentNames({ "appId", "versionId"})
+	public void downloadFromHockeyApp(String appId, String versionId){
+		
+		String defaultAPIToken = "19c4f87dde6444e89388a33b1077624e";
+		downloadFromHockeyApp(appId, versionId, defaultAPIToken);
+	}
+	
+	@RobotKeywordOverload
+	@ArgumentNames({ "appId", "versionId","apiToken=19c4f87dde6444e89388a33b1077624e"})
+	public void downloadFromHockeyApp(String appId, String versionId, String apiToken){
+		downloadFromHockeyApp(appId, versionId, apiToken, null);
+	}
+
+	@RobotKeywordOverload
+	@ArgumentNames({ "appId", "versionId","apiToken=19c4f87dde6444e89388a33b1077624e", "appPath=NONE"})
+	public void downloadFromHockeyApp(String appId, String versionId,
+			String apiToken, String appPath){
+		downloadFromHockeyApp(appId, versionId, apiToken, appPath, null);
+	}
+	
+	@RobotKeyword
+	@ArgumentNames({ "appId", "versionId","apiToken=19c4f87dde6444e89388a33b1077624e", "appPath=NONE", "appFileName=NONE" })
+	public void downloadFromHockeyApp(String appId, String versionId,
+			String apiToken, String appPath, String appFileName) {
+		FileOutputStream fos;
+		String unfilteredURL = getHockeyAppDownloadURL(appId, versionId, apiToken);
+		try {
+			String fileExtension = "";
+			if (appFileName == null || appFileName.isEmpty()){
+				fileExtension = unfilteredURL.substring(unfilteredURL.lastIndexOf("/")+1);
+			}else{
+				fileExtension = appFileName.substring(appFileName.lastIndexOf(".") + 1);
+			}
+			
+			//Remove the sneaky /appExtension at the end and change download link so that it actually contains the file.
+			String filteredURL = unfilteredURL.substring(0,unfilteredURL.lastIndexOf("/")).replace("/apps/", "/api/2/apps/");
+			
+			String baseURL = filteredURL + "?format="+fileExtension+"&avtoken=" + apiToken;
+			System.out.println(baseURL);
+			HttpURLConnection con = (HttpURLConnection) new URL(baseURL).openConnection();
+
+			int responseCode = con.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				
+				// If appPath is not specified
+				if (appPath == null || appPath.isEmpty()) {
+					appPath = System.getProperty("user.dir") + File.separator + "target";
+				}
+				
+				// If filename is not specified
+				if (appFileName == null || appFileName.isEmpty()) {
+					String disposition = con
+							.getHeaderField("Content-Disposition");
+					if (disposition == null
+							|| !disposition.contains("filename=\"")) {
+						// extracts file name from URL
+						appFileName = baseURL.substring(
+								baseURL.lastIndexOf("/") + 1, baseURL.length()) + fileExtension;
+					} else {
+						// extracts file name from header field
+						int index = disposition.indexOf("filename=\"");
+						appFileName = disposition.substring(index + 10, disposition.length() - 1);
+					}
+				}
+				File appFile = new File(appPath + File.separator + appFileName);
+				appFile.getParentFile().mkdirs();
+				ReadableByteChannel rbc = Channels.newChannel(con
+						.getInputStream());
+				fos = new FileOutputStream(appFile);
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} else {
+				System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String getHockeyAppDownloadURL(String appId, String appVersion,
+			String apiToken) {
+		String request = "https://rink.hockeyapp.net/api/2/apps/" + appId
+				+ "/app_versions";
+
+		HashMap<String, String> headers = new HashMap<String, String>();
+		headers.put("X-HockeyAppToken", apiToken);
+
+		String jsonResponse = HttpRequestUtils.getResponse(request, "GET",
+				headers);
+		JSONObject doc = HttpRequestUtils.parseStringIntoJson(jsonResponse);
+		JSONArray arr = (JSONArray) doc.get("app_versions");
+		Iterator<JSONObject> it = arr.iterator();
+		while (it.hasNext()) {
+			JSONObject item = it.next();
+			if (item.containsValue(appVersion)) {
+				if(((String)item.get("title")).contains("Android")){
+					return (String) item.get("download_url") + "/apk";
+				} else {
+					return (String) item.get("download_url") + "/ipa";
+				}
+				
+			}
+		}
+
+		return null;
+	}
+
 	// ##############################
 	// Internal Methods
 	// ##############################
@@ -228,5 +364,5 @@ public class ApplicationManagement extends RunOnFailureKeywordsAdapter {
 	 * logging.warn("Invalid desiredCapabilities: " +
 	 * desiredCapabilitiesString); } } } } return desiredCapabilities; }
 	 */
-	
+
 }
---
+++ src/main/java/abtlibrary/utils/HttpRequestUtils.java
@@ -0,0 +1,54 @@
+package abtlibrary.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class HttpRequestUtils {
+	public static String getResponse(String url, String method, Map<String, String> headers) {
+		StringBuffer response = new StringBuffer();
+
+		try {
+			URL obj;
+			obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			// optional default is GET
+			con.setRequestMethod(method);
+
+			// add request header
+			for (Map.Entry<String, String> cursor : headers.entrySet()){
+				con.setRequestProperty(cursor.getKey(), cursor.getValue());
+			}
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return response.toString();
+	}
+	
+	public static JSONObject parseStringIntoJson(String content){
+		JSONParser parser = new JSONParser();
+		try {
+			return (JSONObject) parser.parse(content);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
